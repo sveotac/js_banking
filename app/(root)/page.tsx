6d41d877fd027c0bbec85e5b7de0c7038c31ce54
sveotac <strahinja.aleksic@hotmail.com>
2024-06-25 13:18:57 +0200

    Implemented the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/HeaderBox'
+import Head from 'next/head'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+    const loggedIn =  { firstName: 'Allfather', lastName: 'ALF', email: 'allfather@valhalla.com'}
+
   return (
-    <div>Home</div>
+   <section className="home">
+    <div className="home-content">
+        <header className="home-header">
+            <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user = {loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your account and transactions efficiently"
+            />
+
+            <TotalBalanceBox
+                accounts={[]}
+                totalBanks={1}
+                totalCurrentBalance={1250.35}
+            />
+        </header>
+
+        RECENT TRANSACTIONS
+    </div>
+
+    <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:123.50}, {currentBalance:500.50}]}
+    />
+   </section>
   )
 }
 
---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Trasnfer = () => {
+  return (
+    <div>Trasnfer</div>
+  )
+}
+
+export default Trasnfer
